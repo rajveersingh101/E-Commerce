879f907a15ed04030094768122cee93d0c07f051
rajveersingh101 <100012728+rajveersingh101@users.noreply.github.com>
2025-09-19 23:10:03 +0530

    Update cart.js

--- cart.js
+++ cart.js
@@ -1,5 +1,4 @@
 console.clear();
-
 // Update badge if items are present
 if (document.cookie.indexOf(',counter=') >= 0) {
     let counter = document.cookie.split(',')[1].split('=')[1];
@@ -9,54 +8,67 @@ if (document.cookie.indexOf(',counter=') >= 0) {
 let cartContainer = document.getElementById('cartContainer');
 let boxContainerDiv = document.createElement('div');
 boxContainerDiv.id = 'boxContainer';
-
 // Create total and button containers
 let totalContainerDiv = document.createElement('div');
 totalContainerDiv.id = 'totalContainer';
 let totalDiv = document.createElement('div');
 totalDiv.id = 'total';
 totalContainerDiv.appendChild(totalDiv);
-
 let totalh2 = document.createElement('h2');
 totalh2.appendChild(document.createTextNode('Total Amount'));
 totalDiv.appendChild(totalh2);
-
 let buttonDiv = document.createElement('div');
 buttonDiv.id = 'button';
 totalDiv.appendChild(buttonDiv);
-
 let buttonTag = document.createElement('button');
 buttonDiv.appendChild(buttonTag);
-
 let buttonLink = document.createElement('a');
 buttonLink.href = '/orderPlaced.html?';
 let buttonText = document.createTextNode('Place Order');
 buttonLink.appendChild(buttonText);
 buttonTag.appendChild(buttonLink);
 
+// Will be filled after reading cart data
+let cartItemsForDataLayer = [];
+
+// Place Order event
 buttonTag.onclick = function () {
     console.log("clicked");
-    // Here you can add GTM/ecommerce Place Order event if needed
+    // GTM begin_checkout
+    window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+        event: "begin_checkout",
+        ecommerce: {
+            items: cartItemsForDataLayer
+        }
+    });
+    // Let the <a> continue to redirect as normal
 };
 
 // Dynamic rendering of cart items
 function dynamicCartSection(ob, itemCounter) {
     let boxDiv = document.createElement('div');
     boxDiv.id = 'box';
-
     let boxImg = document.createElement('img');
     boxImg.src = ob.preview;
     boxDiv.appendChild(boxImg);
-
     let boxh3 = document.createElement('h3');
     boxh3.appendChild(document.createTextNode(ob.name + ' × ' + itemCounter));
     boxDiv.appendChild(boxh3);
-
     let boxh4 = document.createElement('h4');
     boxh4.appendChild(document.createTextNode('Amount: Rs ' + ob.price * itemCounter));
     boxDiv.appendChild(boxh4);
-
     boxContainerDiv.appendChild(boxDiv);
+
+    // Add to the cart array for data layer
+    cartItemsForDataLayer.push({
+        item_id: ob.id,
+        item_name: ob.name,
+        item_brand: ob.brand,
+        price: ob.price,
+        quantity: itemCounter,
+        item_image: ob.preview
+    });
 }
 
 // Function to update total amount
@@ -82,6 +94,7 @@ httpRequest.onreadystatechange = function () {
             let item = document.cookie.split(',')[0].split('=')[1].split(" ");
             let totalAmount = 0;
 
+            // Build deduplicated item/quantity pairs for UI and data layer
             for (let i = 0; i < counter; i++) {
                 let itemCounter = 1;
                 for (let j = i + 1; j < counter; j++) {
@@ -94,6 +107,15 @@ httpRequest.onreadystatechange = function () {
                 i += (itemCounter - 1);
             }
             amountUpdate(totalAmount);
+
+            // GTM view_cart event
+            window.dataLayer = window.dataLayer || [];
+            window.dataLayer.push({
+                event: "view_cart",
+                ecommerce: {
+                    items: cartItemsForDataLayer
+                }
+            });
         } else {
             console.log('call failed!');
         }
